8f2ed5aa672c3d167dd2878a0e5adcb733984020
Krecsa <malistova02@mail.ru>
2024-09-03 20:14:57 +0300

    Отзывы (пока только страница)

--- fishop/ishop/models.py
+++ fishop/ishop/models.py
@@ -6,9 +6,17 @@ class Product(models.Model):
     price = models.IntegerField()
     image_url = models.CharField(max_length=1024)
 
-
     dpi = models.IntegerField(null=True, blank=True)
     type_k = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
         return self.name
+
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    author = models.CharField(max_length=256)
+    rating = models.IntegerField()
+    usage_duration = models.IntegerField()
+    text = models.TextField()
