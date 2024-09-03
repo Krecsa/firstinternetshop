3e4418b598d33f9bf3a622d2ef7015e2412e98c7
Krecsa <malistova02@mail.ru>
2024-09-03 19:53:57 +0300

    Строка поиска (теперь работает)

--- fishop/ishop/views.py
+++ fishop/ishop/views.py
@@ -3,9 +3,19 @@ from django.shortcuts import render
 from .models import Product
 
 def home(request):
-    products = Product.objects.all()
+    search = request.GET.get('search')
 
-    return render(request, 'index.html', {'products': products})
+    if search:
+        products = Product.objects.filter(name__contains=search).all()
+    else:
+        products = Product.objects.all()
+
+    return render(request, "index.html", {
+        'products': products,
+        'products_found': len(products) > 0,
+        'search': search if search else '',
+    })
+
 
 def view_product(request, id):
     product = Product.objects.filter(id=id).first()
